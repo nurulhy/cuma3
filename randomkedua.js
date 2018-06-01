bb72c6f66852a97bc5b99fa5364ee997597beb26
nurulhy <33342511+nurulhy@users.noreply.github.com>
2018-06-01 16:32:55 +0700

    Update randomkedua.js

--- randomkedua.js
+++ randomkedua.js
@@ -24,7 +24,8 @@ var xxx=null;
 if(x=="1"){xxx="/disability-insurance/"}
 if(x=="2"){xxx="/the-importance-of-having-a-will/"}
 if(x=="3"){xxx="/how-you-your-partner-should-save-money/"}
-if(x=="4"){xxx="/become-genius-with-foods/"}a_to_ve[i].href="http://rekosite.epizy.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="/become-genius-with-foods/"}
+if(x=="5"){xxx="/how-to-remove-acne/"}a_to_ve[i].href="http://rekosite.epizy.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
